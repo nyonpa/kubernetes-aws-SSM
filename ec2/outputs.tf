1a5135e1cbe8131979c799044649d02cd594f8f8
nyonpa <tshering.namgyal.dorji@gmail.com>
2026-05-26 23:28:37 +0600

    foreach output

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,11 +1,11 @@
 output "ec2_public_ip" {
     # while using meta arguments
     #value = aws_instance.my_instance[*].public_ip
-    value = aws_instance.my_instance.public_ip
+    value = [ for key in aws_instance.my_instance : key.public_ip]
   
 }
 
 output "ec2_public_dns"{
     #value = aws_instance.my_instance[*].public_dns
-    value = aws_instance.my_instance.public_dns 
+    value = [for key in aws_instance.my_instance : key.public_dns ]
 }
